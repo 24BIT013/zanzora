b67b3b488f814b3df3dd0cc3834df6789aba3df0
24BIT013 <burminho098@gmail.com>
2026-09-25 10:33:17 +0300

    1

--- zanzora/urls.py
+++ zanzora/urls.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 
 urlpatterns = [path("admin/", admin.site.urls), path("", include("explore.urls"))]
+
+# Fallback static-file route for the Render Gunicorn deployment.
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
